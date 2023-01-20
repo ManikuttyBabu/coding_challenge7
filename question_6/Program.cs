83842283112bb72b458b3fc6d06cb1247338014d
Manikuutty Babu <manikuutty.babu@experionglobal.local>
2023-01-20 11:43:54 +0530

    first commit

---
+++ question_6/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("Enter the name of the passenger : ");
+string names = Console.ReadLine();
+
+Console.WriteLine("Enter total miles travelled : ");
+double total_miles = Convert.ToDouble(Console.ReadLine());
+
+if (total_miles > 10000 && total_miles < 20000)
+{
+    Console.WriteLine(names+" travelled "+total_miles+" and Award 10 frequent flyer points ");
+}
+else if (total_miles > 20000 && total_miles < 50000)
+{
+    Console.WriteLine(names+" travelled "+total_miles+" and Award 20 frequent flyer points ");
+}
+else if (total_miles > 50000 && total_miles < 100000)
+{
+    Console.WriteLine(names+" travelled "+total_miles+" and Award 30 frequent flyer points ");
+}
+else if (total_miles > 100000)
+{
+    Console.WriteLine(names+" travelled "+total_miles+" and Award 50 frequent flyer points ");
+}
+else
+{
+    Console.WriteLine(names+" travelled "+total_miles+"  and Nooo flyer points ");
+}
+
+
+
